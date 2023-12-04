54807464186f102fd507a0a37c7c0c6cfa1e822a
Korron <lkorron@yandex.ru>
2023-12-04 15:28:21 +0300

    comments

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class FN(nn.Module):
 
 # black-box (используется при тестировании, нельзя посчитать градиент)
 def create_black_box(param_range=5):
-    a, b, c = (random.randint(1, 10),
+    a, b, c = (random.randint(1, param_range * 2),
                random.randint(-param_range, param_range),
                random.randint(-param_range, param_range))
     return lambda x: a * ((x - b) ** 2) + c, (a, b, c)
@@ -59,26 +59,19 @@ def train(model, criterion, optimizer, input, target, hidden_size, rnn_iteration
     model.train()
     optimizer.zero_grad()
 
-    # инициализация начального состояния rnn
     x, fn = input
     y = fn(x)
     hidden = init_hidden(hidden_size)
 
     timesteps = rnn_iterations
 
-    # форвард-степ по всем итерациям rnn
     for _ in range(timesteps):
         x, y, hidden = model(fn, x, y, hidden)
 
-    # расчет лосса, градиента, градиентный шаг
     loss = criterion(x, target)
     loss.backward()
     optimizer.step()
 
-    # проверка нормы градиента
-    # print(get_norm(model))
-
-    # ограничение нормы градиента
     max_norm = 20.
     nn.utils.clip_grad_norm_(model.parameters(), max_norm)
 
@@ -105,14 +98,10 @@ def main():
     dataset_size = 10000
 
     for i in range(dataset_size):
-        # генерация параметров white-box
         x_opt = 0
         f_opt = random.randint(-searching_range, searching_range)
 
-        # генерация начальной точки, white-box
         x_initial = torch.tensor([random.uniform(-searching_range, searching_range)])
-
-        old = torch.randn(1) * 10
 
         input = (x_initial, FN(x_opt, f_opt))
 
