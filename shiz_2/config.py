d8f6ce061f95a614067d64b1fc46e77cb1b8e367
Korron <lkorron@yandex.ru>
2024-04-11 12:35:17 +0300

    new layers

--- shiz_2/config.py
+++ shiz_2/config.py
@@ -1,8 +1,8 @@
 config = {
     "dimension": 10,
     "lr": 3e-4,
-    "batch": 128,
-    "hidden": 128,
+    "batch": 10240,
+    "hidden": 1024,
     "epoch": 5000,
     "num_batches": 10,
     "patience": 50
