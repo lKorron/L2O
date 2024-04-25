ca7344caff86e81bde8c4c6ce0413e2aad68bb0e
LiceyMaxim <maks.semyonov.2004@gmail.com>
2024-04-25 10:32:57 +0300

    Nevergrad

--- nevergrad_test.py
+++ nevergrad_test.py
@@ -3,6 +3,7 @@ import nevergrad as ng
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+
 # from functions import F4, F5
 from tqdm import tqdm
 from model import CustomLSTM
@@ -13,6 +14,7 @@ import pandas as pd
 import random
 import math
 
+
 class F4:
     def __init__(self):
         self.x_opt = None
@@ -20,7 +22,9 @@ class F4:
         self.coefs2 = None
 
     def forward(self, x):
-        scaled_diffs = np.multiply((x - self.x_opt) ** 2, self.coefs1) + np.multiply(np.abs((x - self.x_opt)), self.coefs2)
+        scaled_diffs = np.multiply((x - self.x_opt) ** 2, self.coefs1) + np.multiply(
+            np.abs((x - self.x_opt)), self.coefs2
+        )
         return np.sum(scaled_diffs, axis=1).reshape(-1, 1)
 
     def generate(self, batch_size: int, dimension: int) -> np.ndarray:
@@ -28,9 +32,29 @@ class F4:
         self.coefs1 = np.random.rand(batch_size, dimension) * 10
         self.coefs2 = np.random.rand(batch_size, dimension) * 10
         return self.forward(self.x_opt)
-    
+
     def __call__(self, x):
         return self.forward(x)
+
+
+class F5:
+    def __init__(self):
+        self.x_opt = None
+        self.coefs1 = None
+        self.coefs2 = None
+
+    def forward(self, x):
+        scaled_diffs = np.multiply(np.abs((x - self.x_opt)), self.coefs2)
+        return np.sum(scaled_diffs, axis=1).reshape(-1, 1)
+
+    def generate(self, batch_size: int, dimension: int) -> np.ndarray:
+        self.x_opt = np.random.rand(batch_size, dimension) * 100 - 50
+        self.coefs2 = np.random.rand(batch_size, dimension) * 10
+        return self.forward(self.x_opt)
+
+    def __call__(self, x):
+        return self.forward(x)
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -59,59 +83,34 @@ for _ in range(test_size):
 
 x_initial = torch.ones(batch_size, DIMENSION).to(device)
 
-# test
-# model.load_state_dict(torch.load("best_model.pth", map_location=torch.device('cpu')))
-
-# x_initial = torch.ones(test_batch_size, DIMENSION).to(device)
-
-# x_axis = []
-# y_axis = []
-# best_y_axis = []
-
-# GRU.batch_size = 1
-
-# with torch.no_grad():
-#     for test_fn, test_f_opt in test_data:
-#         x = x_initial.clone().detach().to(device)
-#         y = test_fn(x)
-
-#         # для сравнения включим первую (статичную) точку
-#         x_axis.append(0)
-#         y_axis.append((y - test_f_opt).mean().item())
-#         best_y_axis.append((y - test_f_opt).mean().item())
-
-#         hidden = model.init_hidden(test_batch_size, device)
-#         c = None
-#         best_y = y
-#         for iteration in range(1, opt_iterations + 1):
-#             new_x, hidden, c = model(x, y, hidden, c)
-#             new_y = test_fn(new_x)
-
-#             x = new_x
-#             y = new_y.forward()
-
 x_axis = []
 best_y_axis = []
+y_axis = []
 
-optimizer = ng.optimizers.NGOpt(parametrization=4, budget=opt_iterations + 1)
+# print("\n".join(sorted(ng.optimizers.registry.keys())))
+
+params = ng.p.Instrumentation(ng.p.Array(shape=(4,), lower=-50, upper=50))
 
 with torch.no_grad():
     for test_fn, test_f_opt in tqdm(test_data):
 
+        optimizer = ng.optimizers.CMA(
+            parametrization=params, budget=opt_iterations + 1
+        )
+        # optimizer = ng.optimizers.BayesOptim(
+        #     parametrization=params, budget=opt_iterations + 1
+        # )
         best_y = float("+inf")
-
         for i in range(optimizer.budget):
 
             x = optimizer.ask()
-            y = test_fn(*x.args, **x.kwargs)
+            y = test_fn(*x.args)
             optimizer.tell(x, y)
 
             x_axis.append(i)
             best_y = min(best_y, y)
-            # print(best_y)
+            y_axis.append((y - test_f_opt).item())
             best_y_axis.append((best_y - test_f_opt).item())
-
-        # break
 
 min_df = pd.DataFrame(
     {
@@ -122,9 +121,52 @@ min_df = pd.DataFrame(
     }
 )
 fig_min, ax_min = plt.subplots(figsize=(20, 5))
-gfg_min = sns.boxplot(x="Iteration", y="Loss log (min(y_k where k <= i) - y_best)", data=min_df, ax=ax_min)
+gfg_min = sns.boxplot(
+    x="Iteration", y="Loss log (min(y_k where k <= i) - y_best)", data=min_df, ax=ax_min
+)
 plt.title("Boxplot of Losses by best value on each iteration")
-plt.savefig(f"random_{DIMENSION}_hard.png")
+plt.savefig(f"nevergrad_{DIMENSION}.png")
+plt.show()
+
+# loss_df = pd.DataFrame(
+#     {
+#         "Iteration": x_axis,
+#         "Loss log (y_i - y_best)": torch.log10(torch.tensor(y_axis)),
+#     }
+# )
+
+# fig, ax = plt.subplots(figsize=(20, 5))
+# gfg = sns.boxplot(x="Iteration", y="Loss log (y_i - y_best)", data=loss_df, ax=ax)
+# plt.title("Boxplot of Losses by value on each iteration")
+# plt.show()
+
+x_axis = []
+best_y_axis = []
+
+with torch.no_grad():
+    for test_fn, test_f_opt in test_data:
+        best_y = float("+inf")
+        for iteration in range(opt_iterations + 1):
+            x = np.random.uniform(-50, 50, size=4)
+            y = test_fn(x)
+            best_y = min(best_y, y)
+            x_axis.append(iteration)
+            best_y_axis.append((best_y - test_f_opt).item())
+
+min_df = pd.DataFrame(
+    {
+        "Iteration": x_axis,
+        "Loss log (min(y_k where k <= i) - y_best)": torch.log10(
+            torch.tensor(best_y_axis)
+        ),
+    }
+)
+fig_min, ax_min = plt.subplots(figsize=(20, 5))
+gfg_min = sns.boxplot(
+    x="Iteration", y="Loss log (min(y_k where k <= i) - y_best)", data=min_df, ax=ax_min
+)
+plt.title("Boxplot of Losses by best value on each iteration")
+plt.savefig(f"ones_{DIMENSION}.png")
 plt.show()
 
 # for test_fn, test_f_opt in test_data:
