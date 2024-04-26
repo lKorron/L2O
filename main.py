2c6b76e9150b304e21af3cb2559ab00c0b87a9c2
LiceyMaxim <maks.semyonov.2004@gmail.com>
2024-04-26 12:50:44 +0300

    l2o 25.04

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import torch
 from torch import nn
-import matplotlib.pyplot as plt
-from functions import F4, F5
+from functions import F4
 from model import CustomLSTM
 from config import config
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import wandb
 import random
-import math
+import numpy as np
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -47,12 +44,10 @@ def train(model, optimizer, x, fn, target, opt_iterations):
 
     for _ in range(opt_iterations):
         new_x, hidden, c = model(x, y, hidden, c)
-
         new_y = fn(new_x)
         x = new_x
         y = new_y
         loss = criterion(target, new_y)
-
         total_loss += loss
 
     total_loss.backward()
@@ -71,7 +66,7 @@ learning_rate = config["lr"]
 batch_size = config["batch"]  # размер батча
 num_batches = config["num_batches"]  # количество батчей в эпохе
 num_epoch = config["epoch"]  # количество эпох
-test_size = 1000  # количество тестовых функций
+test_size = 100  # количество тестовых функций
 test_batch_size = 1
 
 model = CustomLSTM(input_size, config["hidden"])
@@ -112,74 +107,76 @@ num_iter = 1
 
 x_initial = torch.ones(batch_size, DIMENSION).to(device)
 
-for epoch in range(num_epoch):
-    # train
-    model.train()
-    epoch_train_loss = 0
-    random.shuffle(train_data)
-    for fn, f_opt in train_data:
-        target = f_opt
-        loss = train(model, optimizer, x_initial, fn, target, opt_iterations)
-        summ += loss
-        epoch_train_loss += loss / batch_size
-        losses.append(summ / num_iter)
-        num_iter += 1
-        wandb.log({"train_loss": losses[-1]})
+train_flag = False
 
-    wandb.log({"epoch_train_loss": epoch_train_loss})
+if train_flag:
+    for epoch in range(num_epoch):
+        # train
+        model.train()
+        epoch_train_loss = 0
+        random.shuffle(train_data)
+        for fn, f_opt in train_data:
+            target = f_opt
+            loss = train(model, optimizer, x_initial, fn, target, opt_iterations)
+            summ += loss
+            epoch_train_loss += loss / batch_size
+            losses.append(summ / num_iter)
+            num_iter += 1
+            wandb.log({"train_loss": losses[-1]})
 
-    # val
-    model.eval()
-    epoch_val_loss = 0
-    with torch.no_grad():
-        for val_fn, val_f_opt in val_data:
-            criterion = IterationWeightedLoss()
-            x = x_initial.clone().detach()
-            x = x.to(device)
-            y = val_fn(x)
-            hidden = model.init_hidden(batch_size, device)
-            c = None
-            total_loss = torch.tensor([0.0]).to(device)
+        wandb.log({"epoch_train_loss": epoch_train_loss})
 
-            for _ in range(opt_iterations):
-                new_x, hidden, c = model(x, y, hidden, c)
-                new_y = val_fn(new_x)
+        # val
+        model.eval()
+        epoch_val_loss = 0
+        with torch.no_grad():
+            for val_fn, val_f_opt in val_data:
+                criterion = IterationWeightedLoss()
+                x = x_initial.clone().detach()
+                x = x.to(device)
+                y = val_fn(x)
+                hidden = model.init_hidden(batch_size, device)
+                c = None
+                total_loss = torch.tensor([0.0]).to(device)
 
-                x = new_x
-                y = new_y
+                for _ in range(opt_iterations):
+                    new_x, hidden, c = model(x, y, hidden, c)
+                    new_y = val_fn(new_x)
 
-                loss = criterion(val_f_opt, new_y)
-                total_loss += loss
+                    x = new_x
+                    y = new_y
 
-            epoch_val_loss += total_loss.item() / batch_size
+                    loss = criterion(val_f_opt, new_y)
+                    total_loss += loss
 
-    wandb.log({"epoch_val_loss": epoch_val_loss})
+                epoch_val_loss += total_loss.item() / batch_size
 
-    # ранняя остановка
-    if epoch_val_loss < best_val_loss:
-        print(f"[{epoch}] [****] Train {epoch_train_loss} Valid {epoch_val_loss}")
-        best_val_loss = epoch_val_loss
-        epochs_no_improve = 0
-        torch.save(model.state_dict(), "best_model.pth")
-    else:
-        print(f"[{epoch}] [////] Train {epoch_train_loss} Valid {epoch_val_loss}")
-        epochs_no_improve += 1
-        if epochs_no_improve >= patience:
-            print(
-                f"Early stopping at epoch {epoch} due to no improvement in validation loss."
-            )
-            break
+        wandb.log({"epoch_val_loss": epoch_val_loss})
+
+        # ранняя остановка
+        if epoch_val_loss < best_val_loss:
+            print(f"[{epoch}] [****] Train {epoch_train_loss} Valid {epoch_val_loss}")
+            best_val_loss = epoch_val_loss
+            epochs_no_improve = 0
+            torch.save(model.state_dict(), "best_model.pth")
+        else:
+            print(f"[{epoch}] [////] Train {epoch_train_loss} Valid {epoch_val_loss}")
+            epochs_no_improve += 1
+            if epochs_no_improve >= patience:
+                print(
+                    f"Early stopping at epoch {epoch} due to no improvement in validation loss."
+                )
+                break
 
 # test
-# model.load_state_dict(torch.load("best_model.pth", map_location=torch.device('cpu')))
+
+model.load_state_dict(torch.load("best_model.pth", map_location=torch.device('cpu')))
 
 x_initial = torch.ones(test_batch_size, DIMENSION).to(device)
 
 x_axis = []
 y_axis = []
 best_y_axis = []
-
-# GRU.batch_size = 1
 
 with torch.no_grad():
     for test_fn, test_f_opt in test_data:
@@ -207,66 +204,4 @@ with torch.no_grad():
             y_axis.append(loss.item())
             best_y_axis.append((best_y - test_f_opt).item())
 
-# боксплоты по итерациям
-
-loss_df = pd.DataFrame(
-    {
-        "Iteration": x_axis,
-        "Loss log (y_i - y_best)": torch.log10(torch.tensor(y_axis)),
-    }
-)
-
-min_df = pd.DataFrame(
-    {
-        "Iteration": x_axis,
-        "Loss log (min(y_k where k <= i) - y_best)": torch.log10(
-            torch.tensor(best_y_axis)
-        ),
-    }
-)
-
-torch.rand(1, DIMENSION, device=device) * 100 - 50
-
-
-fig, ax = plt.subplots(figsize=(20, 5))
-gfg = sns.boxplot(x="Iteration", y="Loss log (y_i - y_best)", data=loss_df, ax=ax)
-plt.title("Boxplot of Losses by value on each iteration")
-plt.savefig(f"result_{DIMENSION}_module.png")
-plt.show()
-
-fig_min, ax_min = plt.subplots(figsize=(20, 5))
-gfg_min = sns.boxplot(x="Iteration", y="Loss log (min(y_k where k <= i) - y_best)", data=min_df, ax=ax_min)
-plt.title("Boxplot of Losses by best value on each iteration")
-plt.savefig(f"result_{DIMENSION}_min_module.png")
-plt.show()
-
-# x_axis = []
-# best_y_axis = []
-
-# with torch.no_grad():
-#     for test_fn, test_f_opt in test_data:
-#         x = x_initial.clone().detach().to(device)
-#         y = test_fn(x)
-#         x_axis.append(0)
-#         best_y_axis.append((y - test_f_opt).mean().item())
-#         best_y = y
-#         for iteration in range(1, opt_iterations + 1):
-#             x = torch.rand(1, DIMENSION, device=device) * 100 - 50
-#             y = test_fn(x)
-#             best_y = min(best_y, y)
-#             x_axis.append(iteration)
-#             best_y_axis.append((best_y - test_f_opt).item())
-
-# min_df = pd.DataFrame(
-#     {
-#         "Iteration": x_axis,
-#         "Loss log (min(y_k where k <= i) - y_best)": torch.log10(
-#             torch.tensor(best_y_axis)
-#         ),
-#     }
-# )
-# fig_min, ax_min = plt.subplots(figsize=(20, 5))
-# gfg_min = sns.boxplot(x="Iteration", y="Loss log (min(y_k where k <= i) - y_best)", data=min_df, ax=ax_min)
-# plt.title("Boxplot of Losses by best value on each iteration")
-# plt.savefig(f"random_{DIMENSION}_hard.png")
-# plt.show()
+np.savez('out_model.npz', x=x_axis, y=best_y_axis)
---
+++ test_nevergrad.py
@@ -0,0 +1,169 @@
+import numpy as np
+import nevergrad as ng
+import torch
+from tqdm import tqdm
+from config import config
+
+
+class F4:
+    def __init__(self):
+        self.x_opt = None
+        self.coefs1 = None
+        self.coefs2 = None
+
+    def forward(self, x):
+        scaled_diffs = np.multiply((x - self.x_opt) ** 2, self.coefs1) + np.multiply(
+            np.abs((x - self.x_opt)), self.coefs2
+        )
+        return np.sum(scaled_diffs, axis=1).reshape(-1, 1)
+
+    def generate(self, batch_size: int, dimension: int) -> np.ndarray:
+        self.x_opt = np.random.rand(batch_size, dimension) * 100 - 50
+        self.coefs1 = np.random.rand(batch_size, dimension) * 10
+        self.coefs2 = np.random.rand(batch_size, dimension) * 10
+        return self.forward(self.x_opt)
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
+class F4Bayes:
+    def __init__(self):
+        self.x_opt = None
+        self.coefs1 = None
+        self.coefs2 = None
+
+    def forward(self, x1, x2, x3, x4):
+        x = np.array([x1, x2, x3, x4], dtype=np.float64)
+        scaled_diffs = np.multiply((x - self.x_opt) ** 2, self.coefs1) + np.multiply(
+            np.abs((x - self.x_opt)), self.coefs2
+        )
+        return np.sum(scaled_diffs, axis=0)
+
+    def generate(self, batch_size: int, dimension: int) -> np.ndarray:
+        self.x_opt = np.random.rand(dimension) * 100 - 50
+        self.coefs1 = np.random.rand(dimension) * 10
+        self.coefs2 = np.random.rand(dimension) * 10
+        return self.forward(self.x_opt[0], self.x_opt[1], self.x_opt[2], self.x_opt[3])
+
+    def __call__(self, x1, x2, x3, x4):
+        return self.forward(x1, x2, x3, x4)
+
+
+DIMENSION = config["dimension"]
+input_size = DIMENSION + 1
+output_size = DIMENSION
+opt_iterations = 2 * DIMENSION + 1
+
+test_size = 100  # количество тестовых функций
+test_batch_size = 1
+
+
+# Методы из коробки
+
+test_data = []
+for _ in range(test_size):
+    fn = F4()
+    test_data.append((fn, fn.generate(test_batch_size, DIMENSION)))
+
+
+x_axis = []
+best_y_axis = []
+
+params = ng.p.Instrumentation(ng.p.Array(shape=(4,), lower=-50, upper=50))
+
+with torch.no_grad():
+    for test_fn, test_f_opt in tqdm(test_data):
+
+        optimizer = ng.optimizers.CMAbounded(
+            parametrization=params, budget=opt_iterations + 1
+        )
+        best_y = float("+inf")
+        for i in range(optimizer.budget):
+
+            x = optimizer.ask()
+            y = test_fn(*x.args)
+            optimizer.tell(x, y)
+
+            x_axis.append(i)
+            best_y = min(best_y, y)
+            best_y_axis.append((best_y - test_f_opt).item())
+
+
+np.savez("cma.npz", x=x_axis, y=best_y_axis)
+
+
+# BayesOptimBO
+
+x_axis = []
+best_y_axis = []
+
+params = ng.p.Instrumentation(ng.p.Array(shape=(4,), lower=-50, upper=50))
+
+with torch.no_grad():
+    for test_fn, test_f_opt in tqdm(test_data):
+
+        # Тут работают BO, BayesOptimBO
+        optimizer = ng.optimizers.BayesOptimBO(
+            parametrization=params, budget=opt_iterations + 1
+        )
+
+        best_y = float("+inf")
+        for i in range(optimizer.budget):
+
+            x = optimizer.ask()
+            y = test_fn(*x.args)
+            # нужно ограничить точность, чтобы оптимизаторы не падали
+            y = float(y)
+            optimizer.tell(x, y)
+
+            x_axis.append(i)
+            best_y = min(best_y, y)
+            best_y_axis.append((best_y - test_f_opt).item())
+
+np.savez("BayesOptimBo.npz", x=x_axis, y=best_y_axis)
+
+
+# BayesianOptimization
+
+test_data = []
+for _ in range(test_size):
+    fn = F4Bayes()
+    test_data.append((fn, fn.generate(test_batch_size, DIMENSION)))
+
+
+pbounds = {'x1': (-50, 50), 'x2': (-50, 50), 'x3': (-50, 50), 'x4': (-50, 50)}
+
+x_axis = []
+best_y_axis = []
+
+with torch.no_grad():
+    for test_fn, test_f_opt in tqdm(test_data):
+
+        fn = lambda x1, x2, x3, x4: -test_fn(x1, x2, x3, x4)
+
+        optimizer = ng.optimizers.BayesianOptimization(
+            f=fn,
+            pbounds=pbounds,
+            random_state=1,
+        )
+
+        optimizer.maximize(
+            init_points=1,
+            n_iter=opt_iterations+1
+        )
+
+        best_y = float("+inf")
+
+        for i, res in enumerate(optimizer.res):
+
+            xs = optimizer.res[i]['params'].values()
+            value = test_fn(*xs)
+
+            x_axis.append(i)
+
+            y = test_fn(*xs)
+            best_y = min(y, best_y)
+            best_y_axis.append(best_y - test_f_opt)
+
+np.savez("BayesianOptimization.npz", x=x_axis, y=best_y_axis)
