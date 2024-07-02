402ab40651b2d052f57df99f965989c74079092c
LiceyMaxim <maks.semyonov.2004@gmail.com>
2024-07-02 16:04:50 +0300

    fixes

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 config = {
     # Task
     "dimension": 4,
-    "learn_function": "Rosenbrock",
-    "test_function": "Rosenbrock",
+    "learn_function": "CustomComplexFunction",
+    "test_function": "CustomComplexFunction",
     "budget": 2 * 4 + 2,
-    "lower": -50,
-    "upper": 50,
+    "lower": -5,
+    "upper": 5,
     # Functions number
     "batch": 1024,
     "num_batches": 3,
     "test_size": 100,
     # ML staff
     "train": True,
-    "model": "CustomXLSTM",
-    "layers": 1,
+    "model": "CustomBatchedDropLSTM",
+    "layers": 2,
     "lr": 3e-4,
     "hidden": 512,
-    "epoch": 5000,
+    "epoch": 1,  # 5000
     "patience": 300,
     # Loss
     "last_impact": 2,
--- functions_numpy.py
+++ functions_numpy.py
@@ -26,6 +26,52 @@ class Rastrigin:
         return self.forward(x)
 
 
+class CustomComplexFunction:
+    def __init__(self):
+        self.x_opt = None
+        self.B = None
+        self.C = None
+        self.D = None
+        self.freq1 = None
+        self.freq2 = None
+        self.phase_shift = None
+
+    def forward(self, x):
+        x = np.atleast_2d(x)
+        z = x - self.x_opt
+        term1 = self.B * np.sum(
+            np.sin(self.freq1 * np.pi * z + self.phase_shift), axis=1
+        )
+        term2 = self.C * np.sum(np.exp(self.D * z**2), axis=1)
+        term3 = np.sum(np.log1p(np.abs(z)), axis=1)
+        return (term1 + term2 + term3).reshape(-1, 1)
+
+    def generate(
+        self,
+        batch_size: int,
+        dimension: int,
+    ) -> np.ndarray:
+        """
+        Generate function
+        Save hyperparameters (self.x_opt, self.B, self.C, self.D, self.freq1, self.freq2, self.phase_shift)
+        Return y_opt
+        """
+        self.x_opt = np.random.rand(batch_size, dimension) * (upper - lower) + lower
+
+        # Generate hyperparameters
+        self.B = np.random.rand() * 10
+        self.C = np.random.rand() * 10
+        self.D = np.random.rand()
+        self.freq1 = np.random.randint(1, 10)
+        self.freq2 = np.random.randint(1, 10)
+        self.phase_shift = np.random.rand()
+
+        return self.forward(self.x_opt)
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
 class Sphere_Abs:
     def __init__(self):
         self.x_opt = None
